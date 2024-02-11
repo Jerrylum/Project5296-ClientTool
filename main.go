da51015eaf0dc64f66ec996aa53d7cfe36205034
joelum-hk <joelum-hk@proton.me>
2024-02-11 17:38:37 +0000

    Work in progress

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -17,11 +19,16 @@ type Downloader struct {
 	client *http.Client
 }
 
+type UserRequest struct {
+	url  string
+	dest string
+}
+
 type Resource struct {
 	url           string
+	destPath      string
 	status        ResourceStatus
 	contentLength int // in bytes
-	destPath      string
 	isAcceptRange bool
 }
 
@@ -31,10 +38,11 @@ const (
 	DOWNLOADING
 	DOWNLOADED
 	NOT_AVAILABLE
-	CONNECTION_ERROR
+	CONNECTION_TIMEOUT
+	CONNECTION_REFUSED
 )
 
-func readFileByLine(path string) []string {
+func ReadFileByLine(path string) []string {
 	dat, err := os.ReadFile(path)
 	if err != nil {
 		panic(err)
@@ -50,7 +58,7 @@ func readFileByLine(path string) []string {
 	return rtn
 }
 
-func constructDownloadersFromIpList(ipList []string, numOfConn int) []Downloader {
+func ConstructDownloadersFromIpList(ipList []string, numOfConn int) []Downloader {
 	if len(ipList) == 0 || numOfConn <= 0 {
 		panic("No proxy server or invalid number of connections provided")
 	}
@@ -60,7 +68,7 @@ func constructDownloadersFromIpList(ipList []string, numOfConn int) []Downloader
 
 	for {
 		for _, ip := range ipList {
-			downloaders = append(downloaders, constructDownloaderFromIp(ip))
+			downloaders = append(downloaders, ConstructDownloaderFromIp(ip))
 			i--
 
 			if i == 0 {
@@ -70,13 +78,57 @@ func constructDownloadersFromIpList(ipList []string, numOfConn int) []Downloader
 	}
 }
 
-func constructDownloaderFromIp(ip string) Downloader {
+func ConstructUserRequestsFromStringList(requests []string) []UserRequest {
+	var userRequests []UserRequest
+	for _, request := range requests {
+		rawUrl := ""
+		rawDest := ""
+		if strings.Contains(request, " > ") {
+			split := strings.Split(request, " > ")
+
+			rawUrl = strings.TrimSpace(split[0])
+			rawDest, _ = filepath.Abs(strings.TrimSpace(split[1]))
+		} else {
+			rawUrl = strings.TrimSpace(request)
+			rawDest, _ = filepath.Abs("")
+		}
+
+		rawUrlWithoutFragment, _, _ := strings.Cut(rawUrl, "#")
+		urlObj, err := url.ParseRequestURI(rawUrlWithoutFragment)
+		if err != nil {
+			panic("Error due to parsing url: " + request)
+		}
+		url := urlObj.String()
+		urlFileName := path.Base(url)
+
+		dest := ""
+		info1, err2 := os.Stat(rawDest)
+		if err2 == nil && info1.IsDir() == false {
+			dest = rawDest // overwrite the destination
+		} else if err2 == nil && info1.IsDir() {
+			dest = path.Join(rawDest, urlFileName)
+		} else {
+			rawDestParent := path.Dir(rawDest)
+			err3 := os.MkdirAll(rawDestParent, os.ModePerm)
+			if err3 != nil {
+				panic("Error due to creating directory: " + rawDestParent)
+			}
+			dest = rawDest
+		}
+
+		userRequests = append(userRequests, UserRequest{url: url, dest: dest})
+	}
+
+	return userRequests
+}
+
+func ConstructDownloaderFromIp(ip string) Downloader {
 	url_i := url.URL{}
 	url_proxy, _ := url_i.Parse("http://" + ip + ":3000")
 
 	transport := &http.Transport{}
 	transport.Proxy = http.ProxyURL(url_proxy)                        // set proxy
-	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //set ssl
+	transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true} // set ssl
 
 	client := &http.Client{}
 	client.Transport = transport
@@ -85,27 +137,27 @@ func constructDownloaderFromIp(ip string) Downloader {
 	return Downloader{client: client}
 }
 
-func constructResources(downloaders []Downloader, urlList []string) []Resource {
-	resources := make([]Resource, len(urlList))
+func ConstructResources(downloaders []Downloader, requestList []UserRequest) []Resource {
+	resources := make([]Resource, len(requestList))
 
-	jobs := make([]Job, len(urlList))
-	for i, url := range urlList {
+	jobs := make([]Job, len(requestList))
+	for i, request := range requestList {
 		handleI := i
-		handleUrl := url
+		handleRequest := request
 		jobs[i] = func(downloader *Downloader) {
 			// fmt.Println("Downloading", handleUrl, handleI)
-			resources[handleI] = constructResourceFromURL(downloader, handleUrl)
+			resources[handleI] = ConstructResourceFromURL(downloader, handleRequest)
 		}
 	}
 
-	consumeJobs(downloaders, jobs)
+	ConsumeJobs(downloaders, jobs)
 
 	return resources
 }
 
-func constructResourceFromURL(downloader *Downloader, url string) Resource {
+func ConstructResourceFromURL(downloader *Downloader, request UserRequest) Resource {
 	client := downloader.client
-	req, err := http.NewRequest("HEAD", url, nil)
+	req, err := http.NewRequest("HEAD", request.url, nil)
 
 	if err != nil {
 		panic(err)
@@ -116,8 +168,17 @@ func constructResourceFromURL(downloader *Downloader, url string) Resource {
 		errReason := err.Error()
 		if strings.HasSuffix(errReason, "context deadline exceeded (Client.Timeout exceeded while awaiting headers)") {
 			return Resource{
-				url:           url,
-				status:        CONNECTION_ERROR,
+				url:           request.url,
+				destPath:      request.dest,
+				status:        CONNECTION_TIMEOUT,
+				contentLength: 0,
+				isAcceptRange: false}
+		}
+		if strings.HasSuffix(errReason, "connect: connection refused") {
+			return Resource{
+				url:           request.url,
+				destPath:      request.dest,
+				status:        CONNECTION_REFUSED,
 				contentLength: 0,
 				isAcceptRange: false}
 		}
@@ -128,20 +189,22 @@ func constructResourceFromURL(downloader *Downloader, url string) Resource {
 
 	if resp.StatusCode == 200 {
 		return Resource{
-			url:           url,
+			url:           request.url,
+			destPath:      request.dest,
 			status:        AVAILABLE,
 			contentLength: int(resp.ContentLength),
 			isAcceptRange: resp.Header.Get("Accept-Ranges") == "bytes"}
 	} else {
 		return Resource{
-			url:           url,
+			url:           request.url,
+			destPath:      request.dest,
 			status:        NOT_AVAILABLE,
 			contentLength: 0,
 			isAcceptRange: false}
 	}
 }
 
-func consumeJobs(downloaders []Downloader, jobs []Job) {
+func ConsumeJobs(downloaders []Downloader, jobs []Job) {
 	check := make(chan bool)
 	consumed := 0
 
@@ -181,38 +244,59 @@ func consumeJobs(downloaders []Downloader, jobs []Job) {
 	}
 }
 
+func DownloadResources(downloaders []Downloader, resources []Resource) {
+
+}
+
 func main() {
-	var proxyListPathRaw = flag.String("proxies", "", "The path to a file with a list of proxy server ips separated by linefeed")
-	var urlListPathRaw = flag.String("urls", "", "The path to a file with a list of urls to download separated by linefeed")
-	var numOfConnRaw = flag.Int("connections", 0, "The number of connections in total to download")
+	proxyListPathRaw := flag.String("proxies", "", "The path to a file with a list of proxy server ips, separated by linefeed")
+	requestListPathRaw := flag.String("requests", "", `The path to a file with a list of download requests, separated by linefeed
+Each line can be one of the following formats:
+ - Only the URL
+   e.g. 'http://example.com/file.zip'
+   The file will be saved to the current directory with the same name as the file in the url
+ - URL with existing directory
+   e.g. 'http://example.com/file.zip > /path/to/save/'
+   The file will be saved to the specified existing directory with the same name as the file in the url 
+ - URL with specified file path
+   e.g. 'http://example.com/file.zip > /path/to/save/file.zip'
+   The file will be saved to the specified path
+`)
+	numOfConnRaw := flag.Int("connections", 0, "The number of connections in total to download")
 
 	flag.Parse()
 
-	if *proxyListPathRaw == "" && *urlListPathRaw == "" && *numOfConnRaw == 0 {
+	if *proxyListPathRaw == "" && *requestListPathRaw == "" && *numOfConnRaw == 0 {
 		flag.PrintDefaults()
 		return
 	}
 
 	if *proxyListPathRaw == "" {
-		panic("Please provide a list of proxy servers")
+		fmt.Println("Please provide a list of proxy servers")
+		os.Exit(1)
 	}
 
-	if *urlListPathRaw == "" {
-		panic("Please provide a list of urls to download")
+	if *requestListPathRaw == "" {
+		fmt.Println("Please provide a list of urls to download")
+		os.Exit(1)
 	}
 
 	if *numOfConnRaw == 0 {
-		panic("Please provide the number of connections")
-	} else if *numOfConnRaw < 0 {
-		panic("The number of connections must be greater than 0")
+		fmt.Println("Please provide the number of connections")
+		os.Exit(1)
 	}
 
-	var proxyList []string = readFileByLine(*proxyListPathRaw)
-	var urlList []string = readFileByLine(*urlListPathRaw)
+	if *numOfConnRaw < 0 {
+		fmt.Println("The number of connections must be greater than 0")
+		os.Exit(1)
+	}
 
-	var numOfConn = *numOfConnRaw
-	var downloaders []Downloader = constructDownloadersFromIpList(proxyList, numOfConn)
-	var resources []Resource = constructResources(downloaders, urlList)
+	proxyList := ReadFileByLine(*proxyListPathRaw)
+	requestList := ConstructUserRequestsFromStringList(ReadFileByLine(*requestListPathRaw))
+
+	numOfConn := *numOfConnRaw
+	downloaders := ConstructDownloadersFromIpList(proxyList, numOfConn)
+	resources := ConstructResources(downloaders, requestList)
 
 	fmt.Println(resources)
 
